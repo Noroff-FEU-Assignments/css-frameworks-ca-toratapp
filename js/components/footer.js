1498138e5b9462230f13a215f9970dc32d9c7784
toratapp <73744488+toratapp@users.noreply.github.com>
2022-10-14 23:17:03 +0200

    Working on home and footer

--- js/components/footer.js
+++ js/components/footer.js
@@ -1,6 +1,5 @@
 const footer = document.querySelector("footer");
 
 footer.innerHTML += `<div class="footer__vimeo-youtube"><i class="fa-brands fa-vimeo-v"></i><i class="fa-brands fa-youtube"></i></div>
-
-
-`
+                    <div class="footer__paragraphs"><p>hello@yay.com</p><p>Copyright 2020</p></div>`
+                    
