789255c0456c7a08f417d450537aefcbd0314f09
toratapp <73744488+toratapp@users.noreply.github.com>
2022-10-15 13:49:22 +0200

    Worked on home, started on News

--- js/components/footer.js
+++ js/components/footer.js
@@ -1,5 +1,23 @@
 const footer = document.querySelector("footer");
 
-footer.innerHTML += `<div class="footer__vimeo-youtube"><i class="fa-brands fa-vimeo-v"></i><i class="fa-brands fa-youtube"></i></div>
-                    <div class="footer__paragraphs"><p>hello@yay.com</p><p>Copyright 2020</p></div>`
+footer.innerHTML += `<div class="footer-sm-screens d-lg-none">
+                        <div class="footer__vimeo-youtube">
+                            <i class="fa-brands fa-vimeo-v"></i>
+                            <i class="fa-brands fa-youtube"></i>
+                        </div>
+                        <div class="footer__paragraphs">
+                            <p>hello@yay.com</p>
+                            <p>Copyright 2020</p>
+                        </div>
+                    </div>
+                    <div class="footer-lg-screens d-none d-lg-flex">
+                        <div class="footer__vimeo-youtube-wrapper">
+                            <div class="footer__vimeo-youtube">
+                                <i class="fa-brands fa-vimeo-v"></i>
+                                <i class="fa-brands fa-youtube"></i>
+                            </div>
+                        </div>
+                        <p class="footer__email">hello@yay.com</p>
+                        <p class="footer__copyright">Copyright 2020</p>
+                    </div>`
                     
